100c43c666c591abc407c5fe70d0b8d70e0488cf
Pratyush <25bcs129@iiitdwd.ac.in>
2026-06-05 13:23:17 +0530

    Implementing Register and Login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const authController = require('../controllers/auth.controller')
 
 const router = express.Router()
+
+router.post('/register', authController.userRegisterController)
+router.post('/login', authController.userLoginController)
 
 module.exports = router
