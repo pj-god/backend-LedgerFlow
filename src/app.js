a00f660c5b3926a19f3f86d18848e19bddfaa590
Pratyush <25bcs129@iiitdwd.ac.in>
2026-06-05 12:42:10 +0530

    Detailed user schema - password validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const authRouter = require('./routes/auth.routes')
 
 const app = express()
+
+app.use("/api/auth", authRouter)
 
 module.exports = app
---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type: String,
+        unique : [true, "User already exists with this email"],
+        required : [true, "Email is required for creating a user"],
+        lowercase : true,
+        trim : true,
+        match: [/^\S+@\S+\.\S+$/, "Invalid Email Address"]
+    },
+    username : {
+        type: String,
+        required : [true, "Username is required for creating an account"],
+        unique : [true, "User already exists with this username"],
+        trim: true,
+        minlength: [3, "Username should contain more than 3 characters"],
+        maxlength: [20, "Username should contain less than 20 characters"],
+        match: [/^[a-zA-Z0-9_]+$/, "username can only contain letters, numbers and underscore"]
+    },
+    password : {
+        type: String,
+        required : [true, "Password is required for creating an account"],
+        minlength: [8, "Password should contain more than 6 characters"],
+        select: false
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next()
+    }
+    
+    const hash = await bcrypt.hash(this.password, 10)
+
+    this.password = hash
+
+    return next()
+})
+
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
