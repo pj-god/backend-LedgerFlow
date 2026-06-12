2106c0c75786760c960a7e75b989c325f294b611
Pratyush <25bcs129@iiitdwd.ac.in>
2026-06-12 13:01:53 +0530

    Implementing aggregation pipeline

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -36,26 +36,45 @@ async function createTransaction(req,res){
     })
     if(isTransactionAlreadyExists){
         if(isTransactionAlreadyExists.status === "COMPLETED"){
-            res.status(200).json({
+            return res.status(200).json({
                 message : "Transaction already processed",
                 transaction : isTransactionAlreadyExists
             })
         }
         if(isTransactionAlreadyExists.status === "PENDING"){
-            res.status(202).json({
+            return res.status(202).json({
                 message : "Transaction is still under process"
             })
         }
         if(isTransactionAlreadyExists.status === "FAILED"){
-            res.status(500).json({
+            return res.status(500).json({
                 message : "Transaction processing failed, please retry"
             })
         }
         if(isTransactionAlreadyExists.status === "REVERSED"){
-            res.status(500).json({
+            return res.status(500).json({
                 message : "Transaction was reversed, please retry"
             })
         }
+    }
+
+    if(fromUserAccount.status !== "ACTIVE" ){
+        return res.status(400).json({
+            message : "fromAccount must be ACTIVE to process transaction"
+        })
+    }
+    if(toUserAccount.status !== "ACTIVE" ){
+        return res.status(400).json({
+            message : "toAccount must be ACTIVE to process transaction"
+        })
+    }
+
+    const balance = await fromUserAccount.getBalance()
+
+    if(balance < amount){
+        return res.status(400).json({
+            message: `Insufficient balance in fromAccount. Current Balance is ${balance}. Requested amount is ${amount}`
+        })
     }
 
 }
